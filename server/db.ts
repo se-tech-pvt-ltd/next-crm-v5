ff9493404ef9a8cf0cd1fc0a75f9b3f2856b7c4e
sadiq <sadiq.soomro29@gmail.com>
2025-08-25 13:24:28 +0000

    added

--- server/db.ts
+++ server/db.ts
@@ -2,11 +2,5 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "@shared/schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
-}
-
-export const connection = mysql.createPool(process.env.DATABASE_URL);
+export const connection = mysql.createPool(process.env.DATABASE_URL || "mysql://setcrminternet:password@151.106.112.145:3306/sales-crm");
 export const db = drizzle(connection, { schema, mode: "default" });
